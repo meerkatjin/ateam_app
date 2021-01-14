c3b5254a6a7318b7c438ac9d858a2956a09191d6
302-06\hanul <vesteira2018@gmail.com>
2021-01-14 12:57:29 +0900

    재료리스트 패키지로 담아내기

--- Ateam_app/app/src/main/java/irdnt_list_package/IrdntListFragment.java
+++ Ateam_app/app/src/main/java/irdnt_list_package/IrdntListFragment.java
@@ -1,4 +1,4 @@
-package com.example.ateam_app;
+package irdnt_list_package;
 
 import android.os.Bundle;
 
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.ateam_app.R;
 import com.google.android.material.tabs.TabLayout;
 
 public class IrdntListFragment extends Fragment {
