e54083ffa2b6ade38ae87b29b8b7d74e3067e1a8
302-15\hanul <meerkatjin@naver.com>
2021-01-18 20:12:59 +0900

    처음부터 다시 한다 나

--- Ateam_app/app/src/main/java/com/example/ateam_app/recipe_fragment/RecipeFragment.java
+++ Ateam_app/app/src/main/java/com/example/ateam_app/recipe_fragment/RecipeFragment.java
@@ -19,10 +19,10 @@ import java.util.ArrayList;
 
 
 public class RecipeFragment extends Fragment {
-    RecipeAddapter addapter;
+        RecipeAdapter adapter;
      RecipeDTO dto;
-     RecipeDAO dao;
-     ArrayList<RecipeDAO> recipeDaos;
+
+
      ArrayList<RecipeDTO> dtos;
      RecyclerView recyclerView;
      RecyclerView.LayoutManager mLayoutManager;
@@ -59,20 +59,21 @@ public class RecipeFragment extends Fragment {
 
         dtos = new ArrayList<>();
 
-        addapter = new RecipeAddapter();
+        adapter = new RecipeAdapter();
 
         //activity = (MainActivity) getActivity();
 
-        //dto = new RecipeDTO("김치찜", "김치찜이 짜다", "어렵다", R.drawable.ic_launcher_background);
-        //dtos.add(dto);
+        dto = new RecipeDTO(1, "ㅋ", "ㅋ", "ㅋ", "ㅋ",
+                "ㅋ", "ㅋ", "ㅋ", "ㅋ", "ㅋ", "ㅋ");
+        dtos.add(dto);
 
-        recyclerView.setAdapter(addapter);
+        recyclerView.setAdapter(adapter);
         //addapter.notifyDataSetChanged();
 
-        addapter.setOnItemClicklistener(new OnRecipeItemClickListener() {
+        adapter.setOnItemClicklistener(new OnRecipeItemClickListener() {
             @Override
-            public void onItemClick(RecipeAddapter.ViewHolder holder, View view, int position) {
-                RecipeDTO item = addapter.getItem(position);
+            public void onItemClick(RecipeAdapter.ViewHolder holder, View view, int position) {
+                RecipeDTO item = adapter.getItem(position);
                 Intent intent = new Intent(getContext(), RecipeSubActivity.class);
                 intent.putExtra("img_url", item.getImg_url());
                 startActivity(intent);
