fd8c9af76a2b2ec6d7015e88284604e016c18d04
302-11\hanul <tpfk373@hanmail.net>
2021-01-20 18:41:12 +0900

    회원정보수정 정보뿌림

--- Ateam_app/app/src/main/java/com/example/ateam_app/UserInfoChangeActivity.java
+++ Ateam_app/app/src/main/java/com/example/ateam_app/UserInfoChangeActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +23,7 @@ public class UserInfoChangeActivity extends AppCompatActivity {
     Button btnInfoChange, btnInfoChangeCancel;
 
     String email, pw, name, addr, img, phone;
+    Intent mainIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,26 +39,22 @@ public class UserInfoChangeActivity extends AppCompatActivity {
         user_addr = findViewById(R.id.user_addr);
         user_phone_no = findViewById(R.id.user_phone_no);
 
-        //수정시 이메일 변경불가
-        user_email.setEnabled(false);
+        //보내온 값 파싱
+        mainIntent = getIntent();
+        UserDTO loginDTO = (UserDTO) mainIntent.getSerializableExtra("loginDTO");
+        Log.d("세라수정", "onCreate: email" + loginDTO.getUser_email());
 
-        //보내온값 파싱
-        Intent intent = getIntent();
-        UserDTO userDTO = (UserDTO) intent.getSerializableExtra("userDTO");
+        email = loginDTO.getUser_email();
+        pw = loginDTO.getUser_pw();
+        name = loginDTO.getUser_nm();
+        addr = loginDTO.getUser_addr();
+        phone = loginDTO.getUser_phone_no();
 
-        email = userDTO.getUser_email();
-        pw = userDTO.getUser_pw();
-        name = userDTO.getUser_nm();
-        addr = userDTO.getUser_addr();
-        phone = userDTO.getUser_phone_no();
-
-        //가져온값 써넣기
         user_email.setText(email);
         user_pw.setText(pw);
         user_nm.setText(name);
         user_addr.setText(addr);
         user_phone_no.setText(phone);
-
 
         //수정버튼 누름
         btnInfoChange.setOnClickListener(new View.OnClickListener() {
@@ -85,6 +83,8 @@ public class UserInfoChangeActivity extends AppCompatActivity {
         builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+
+
                 Toast.makeText(UserInfoChangeActivity.this, "수정되었습니다!", Toast.LENGTH_SHORT).show();
 
             }
@@ -133,26 +133,4 @@ public class UserInfoChangeActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    public void btnUpdateClicked(View view){
-        if(isNetworkConnected(this) == true){
-            UserInfoUpdate userInfoUpdate = new UserInfoUpdate(email, pw, name, addr, img, phone);
-            userInfoUpdate.execute();
-
-            Toast.makeText(getApplicationContext(), "수정성공", Toast.LENGTH_LONG).show();
-
-            Intent showIntent = new Intent(getApplicationContext(), MainActivity.class);
-            showIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |   // 이 엑티비티 플래그를 사용하여 엑티비티를 호출하게 되면 새로운 태스크를 생성하여 그 태스크안에 엑티비티를 추가하게 됩니다. 단, 기존에 존재하는 태스크들중에 생성하려는 엑티비티와 동일한 affinity(관계, 유사)를 가지고 있는 태스크가 있다면 그곳으로 새 엑티비티가 들어가게됩니다.
-                    Intent.FLAG_ACTIVITY_SINGLE_TOP | // 엑티비티를 호출할 경우 호출된 엑티비티가 현재 태스크의 최상단에 존재하고 있었다면 새로운 인스턴스를 생성하지 않습니다. 예를 들어 ABC가 엑티비티 스택에 존재하는 상태에서 C를 호출하였다면 여전히 ABC가 존재하게 됩니다.
-                    Intent.FLAG_ACTIVITY_CLEAR_TOP); // 만약에 엑티비티스택에 호출하려는 엑티비티의 인스턴스가 이미 존재하고 있을 경우에 새로운 인스턴스를 생성하는 것 대신에 존재하고 있는 엑티비티를 포그라운드로 가져옵니다. 그리고 엑티비티스택의 최상단 엑티비티부터 포그라운드로 가져올 엑티비티까지의 모든 엑티비티를 삭제합니다.
-            startActivity(showIntent);
-
-            finish();
-        }else{
-            Toast.makeText(this, "인터넷이 연결되어 있지 않습니다.", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public void btnUpdateCancelClicked(View view){
-        finish();
-    }
 }
