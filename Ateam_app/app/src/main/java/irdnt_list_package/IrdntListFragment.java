5ac98a66af410a6d48b801bd3fc936979eb54583
302-06\hanul <vesteira2018@gmail.com>
2021-01-20 19:58:10 +0900

    연결이 왜 안됨

--- Ateam_app/app/src/main/java/irdnt_list_package/IrdntListFragment.java
+++ Ateam_app/app/src/main/java/irdnt_list_package/IrdntListFragment.java
@@ -1,32 +1,43 @@
 package irdnt_list_package;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.example.ateam_app.MainActivity;
 import com.example.ateam_app.R;
+import com.example.ateam_app.user_pakage.LoginActivity;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
 public class IrdntListFragment extends Fragment {
+    private static final String TAG = "IrdntListFragment";
 
+    String state;
     IrdntListAdapter adapter;
     IrdntListDTO dto;
     ArrayList<IrdntListDTO> items;
     RecyclerView irdntRecyclerView;
     RecyclerView.LayoutManager layoutManager;
     TabLayout irdnt_sort_tab, irdnt_sort_type_tab;
-
+    Button btnInputTest, btnIrdntInsert, btnIrdntCancel;
+    FrameLayout irdnt_input_frame;
+    EditText content_nm;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -112,10 +123,87 @@ public class IrdntListFragment extends Fragment {
 
         adapter.notifyDataSetChanged();
 
+        btnInputTest = rootView.findViewById(R.id.btnInputTest);
+        irdnt_input_frame = rootView.findViewById(R.id.irdnt_input_frame);
+        btnInputTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                irdnt_input_frame.setVisibility(View.VISIBLE);
+                btnInputTest.setVisibility(View.GONE);
+            }
+        });
 
+        btnIrdntInsert = rootView.findViewById(R.id.btnIrdntInsert);
+        btnIrdntCancel = rootView.findViewById(R.id.btnIrdntCancel);
+        content_nm = rootView.findViewById(R.id.content_nm);
+        btnIrdntInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (content_nm.getText().toString().trim().equals("")) {
+                    Toast.makeText(rootView.getContext(), "재료이름을 입력해주세요", Toast.LENGTH_SHORT).show();
+                    content_nm.requestFocus();
+                }  else {
+                    String name = content_nm.getText().toString().trim();
+
+                    irdntInsertConfirm(name);
+                }
+            }
+        });
+
+        btnIrdntCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                irdnt_input_frame.setVisibility(View.GONE);
+                btnInputTest.setVisibility(View.VISIBLE);
+            }
+        });
 
         return rootView;
     }//onCreateView()
+
+    private void irdntInsertConfirm(String name) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle("재료 추가");
+        builder.setMessage("재료를 추가하시겠습니까?");
+        builder.setIcon(android.R.drawable.ic_dialog_alert);
+
+        builder.setPositiveButton("네", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                IrdntListInsert insert = new IrdntListInsert(name);
+                try {
+                    state = insert.execute().get().trim();
+                    Log.d("main:irdntFragment : ", state);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                if (state.equals("1")) {
+                    Toast.makeText(getActivity().getApplicationContext(), "추가되었습니다!", Toast.LENGTH_SHORT).show();
+                    irdnt_input_frame.setVisibility(View.GONE);
+                    btnInputTest.setVisibility(View.VISIBLE);
+                } else {
+                    Toast.makeText(getActivity().getApplicationContext(), "추가 실패했습니다", Toast.LENGTH_SHORT).show();
+                    irdnt_input_frame.setVisibility(View.GONE);
+                    btnInputTest.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity().getApplicationContext(), "추가 취소했습니다", Toast.LENGTH_SHORT).show();
+                irdnt_input_frame.setVisibility(View.GONE);
+                btnInputTest.setVisibility(View.VISIBLE);
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 
     public static IrdntListFragment newInstance() {
         return new IrdntListFragment();
---
+++ Ateam_app/app/src/main/java/irdnt_list_package/IrdntListInsert.java
@@ -0,0 +1,85 @@
+package irdnt_list_package;
+
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.android.internal.http.multipart.MultipartEntity;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+import static com.example.ateam_app.common.CommonMethod.ipConfig;
+
+public class IrdntListInsert extends AsyncTask<Void, Void, String> {
+    private static final String TAG = "IrdntListInsert";
+    private String content_nm;
+
+    public IrdntListInsert(String name) { content_nm = name; }
+
+    String state = "";
+
+    HttpClient httpClient;
+    HttpPost httpPost;
+    HttpResponse httpResponse;
+    HttpEntity httpEntity;
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        try {
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+            builder.setCharset(Charset.forName("UTF-8"));
+
+            builder.addTextBody("content_nm", content_nm, ContentType.create("Multipart/related", "UTF-8"));
+
+            String postURL = ipConfig + "/ateamappspring/insert";
+
+            InputStream inputStream = null;
+            httpClient = AndroidHttpClient.newInstance("Android");
+            httpPost = new HttpPost(postURL);
+            httpPost.setEntity(builder.build());
+            httpResponse = httpClient.execute(httpPost);
+            httpEntity = httpResponse.getEntity();
+            inputStream = httpEntity.getContent();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null){
+                stringBuilder.append(line + "\n");
+            }
+            state = stringBuilder.toString();
+
+            inputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(httpEntity != null){
+                httpEntity = null;
+            }
+            if(httpResponse != null){
+                httpResponse = null;
+            }
+            if(httpPost != null){
+                httpPost = null;
+            }
+            if(httpClient != null){
+                httpClient = null;
+            }
+        }
+
+        return state;
+    }
+}
