657d830a056c183681d8422c11dda914f77b88f6
302-02\hanul <changho4291@gmail.com>
2021-01-22 15:24:07 +0900

    회원탈퇴기능 구현 완료? 아마도?

---
+++ Ateam_app/app/src/main/java/com/example/ateam_app/user_pakage/atask/UserDelete.java
@@ -0,0 +1,104 @@
+package com.example.ateam_app.user_pakage.atask;
+
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.example.ateam_app.user_pakage.dto.UserDTO;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+import static com.example.ateam_app.common.CommonMethod.ipConfig;
+
+public class UserDelete extends AsyncTask<Void, Void, String> {
+
+    private UserDTO dto;
+
+    public UserDelete(UserDTO dto){
+        this.dto = dto;
+    }
+
+    // 데이터베이스에 삭제결과 0보다크면 삭제성공, 같거나 작으면 실패
+    String state = "";
+
+    HttpClient httpClient;
+    HttpPost httpPost;
+    HttpResponse httpResponse;
+    HttpEntity httpEntity;
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        try {
+            // MultipartEntityBuild 생성
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+            builder.setCharset(Charset.forName("UTF-8"));
+
+            // 문자열 및 데이터 추가
+            if(dto.getUser_type().equals("nomal")){
+                builder.addTextBody("user_id", String.valueOf(dto.getUser_id()), ContentType.create("Multipart/related", "UTF-8"));
+                builder.addTextBody("user_pw", dto.getUser_pw(), ContentType.create("Multipart/related", "UTF-8"));
+                builder.addTextBody("user_type", dto.getUser_type(), ContentType.create("Multipart/related", "UTF-8"));
+            }else{
+                builder.addTextBody("user_id", String.valueOf(dto.getUser_id()), ContentType.create("Multipart/related", "UTF-8"));
+                builder.addTextBody("user_type", dto.getUser_type(), ContentType.create("Multipart/related", "UTF-8"));
+            }
+            Log.d("탈퇴", "doInBackground: " + dto.getUser_id());
+
+            String postURL = ipConfig + "/ateamappspring/appUserDelete";
+            // 전송
+            InputStream inputStream = null;
+            httpClient = AndroidHttpClient.newInstance("Android");
+            httpPost = new HttpPost(postURL);
+            httpPost.setEntity(builder.build());
+            httpResponse = httpClient.execute(httpPost);
+            httpEntity = httpResponse.getEntity();
+            inputStream = httpEntity.getContent();
+
+            // 응답
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null){
+                stringBuilder.append(line + "\n");
+            }
+            state = stringBuilder.toString();
+
+            inputStream.close();
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if(httpEntity != null){
+                httpEntity = null;
+            }
+            if(httpResponse != null){
+                httpResponse = null;
+            }
+            if(httpPost != null){
+                httpPost = null;
+            }
+            if(httpClient != null){
+                httpClient = null;
+            }
+
+        }
+        return state;
+    }
+
+    @Override
+    protected void onPostExecute(String state) {
+
+    }
+}
