8dbe4b6cc32a8de40c068d15bbf94ecd134a54cf
302-15\hanul <meerkatjin@naver.com>
2021-01-27 17:13:28 +0900

    한곳까지

--- Ateam_app/app/src/main/java/com/example/ateam_app/MainFragment.java
+++ Ateam_app/app/src/main/java/com/example/ateam_app/MainFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,47 +22,55 @@ import com.example.ateam_app.recipe_fragment.RecipeAtask;
 import com.example.ateam_app.recipe_fragment.RecipeFragment;
 import com.example.ateam_app.recipe_fragment.RecipeItem;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
 
 import static com.example.ateam_app.common.CommonMethod.isNetworkConnected;
 
 public class MainFragment extends Fragment {
+    private static final String TAG = "ddzgzzg";
     CardView shelfLifeAlertBanner, recipeRecommandBanner, manageTipBanner;
-    Random rnd;
+
     Mainfragment_Recipe_Atask atask;
-    TextView recipe_id;
     ImageView img_url;
     TextView sumry;
-    RecipeItem recipeItem;
-    ArrayList<RecipeItem> recipeItems;
-    RecipeFragment recipeFragment;
+
+    RecipeItem recipeItem ;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
         Context context = getContext();
+        recipeItem = new RecipeItem();
+        if(isNetworkConnected(context) == true) {
+            atask = new Mainfragment_Recipe_Atask(recipeItem);
+            atask.execute();
+        }
+        Log.d(TAG, "onCreateView: "+ recipeItem);
         shelfLifeAlertBanner = rootView.findViewById(R.id.shelfLifeAlertBanner);
         recipeRecommandBanner = rootView.findViewById(R.id.recipeRecommandBanner);
         manageTipBanner = rootView.findViewById(R.id.manageTipBanner);
-        atask = new Mainfragment_Recipe_Atask();
-        recipeItems = new ArrayList<>();
-
 
         img_url = rootView.findViewById(R.id.img_url);
         sumry = rootView.findViewById(R.id.sumry_main);
-
         //랜덤 레시피
-        rnd = new Random();
-        int recipe_id = rnd.nextInt(500);
+        //rnd = new Random();
+        //int recipe_id = rnd.nextInt(499+1);
 
-      /*  if(isNetworkConnected(context) == true) {
-            atask = new Mainfragment_Recipe_Atask(recipeItem, recipe_id);
-            atask.execute();
-
-        }*/
 
         //sumry.setText(recipeItem.getSumry());
+
+
+
+
+
+        //
+
+
 
         //유통기한 알림 배너 클릭 시 유통기한별 정렬 재료 리스트로 이동
         shelfLifeAlertBanner.setOnClickListener(new View.OnClickListener() {
@@ -93,10 +102,6 @@ public class MainFragment extends Fragment {
 
         // Inflate the layout for this fragment
         return rootView;
-    }
-
-    public static MainFragment newInstance() {
-        return new MainFragment();
     }
 
 
